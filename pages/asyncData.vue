cd6d648a51a06ed64c2144b3af3a9839532bc0d9
wikihong <hjpChinash@163.com>
2018-04-25 16:11:31 +0800

    0425-asyncData方法获取数据

---
+++ pages/asyncData.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>姓名:{{info.name}}</h1>
+    <h1>年龄:{{info.age}}</h1>
+    <h1>爱好:{{info.interast}}</h1>
+    {{name}}
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data () {
+      return {
+        name: 'Hello world, wikiHong'
+      }
+    },
+    // promise方法
+    // asyncData () {
+    //   return axios.get('https://api.myjson.com/bins/smjj3')
+    //   .then((res) => {
+    //     return { info: res.data }
+    //   })
+    // },
+    // await方法
+    async asyncData () {
+      let {data} = await axios.get('https://api.myjson.com/bins/smjj3')
+      return {info: data}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
         <li><nuxt-link :to="{name: 'index'}">Home</nuxt-link></li>
         <li><nuxt-link :to="{name: 'about'}">About</nuxt-link></li>
         <li><nuxt-link :to="{name: 'news', params: {newsId: '1108'}}">News</nuxt-link></li>
+        <li><nuxt-link :to="{name: 'asyncData'}">asyncData</nuxt-link></li>
       </ul>
     </div>
   </section>
