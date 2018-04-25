08185364790e346ec2ea866334422fa9204914e4
wikihong <hjpChinash@163.com>
2018-04-25 14:16:25 +0800

    0425-Nuxt的动态路由和参数校验

---
+++ pages/news/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>news-content</h2>
+    <p>newsId: {{ $route.params.id }}</p>
+    <ul>
+      <li><a href="/">Home</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    // 校验参数，如果id是非数字就进入nuxt404页面
+    validate ({params}) {
+      return /^\d+$/.test(params.id)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -4,6 +4,8 @@
     <p>newsId:{{$route.params.newsId}}</p>
     <ul>
       <li><a href="/">News</a></li>
+      <li><a href="/news/123">News-1</a></li>
+      <li><a href="/news/wikiHong">News-2</a></li>
     </ul>
   </div>
 </template>
