6c2fac0b3a89eb0546ec6d7dcc4453bf9dc2b5ac
wikihong <hjpChinash@163.com>
2018-04-25 14:46:16 +0800

    0425-Nuxt的路由动画效

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,14 +2,14 @@
   <div>
     <h2>about Index page</h2>
     <ul>
-      <li><a href="/">Home</a></li>
+      <li><nuxt-link :to="{name: 'index'}">Home</nuxt-link></li>
     </ul>
   </div>
 </template>
 
 <script>
   export default {
-
+    transition: 'test'
   }
 </script>
 
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -3,9 +3,9 @@
     <h2>news Index page</h2>
     <p>newsId:{{$route.params.newsId}}</p>
     <ul>
-      <li><a href="/">News</a></li>
-      <li><a href="/news/123">News-1</a></li>
-      <li><a href="/news/wikiHong">News-2</a></li>
+      <li><nuxt-link :to="{name: 'news'}">News</nuxt-link></li>
+      <li><nuxt-link :to="{name: 'news-id', params: {id: 123}}">News-1</nuxt-link></li>
+      <li><nuxt-link :to="{name: 'news-id', params: {id: 456}}">News-2</nuxt-link></li>
     </ul>
   </div>
 </template>
