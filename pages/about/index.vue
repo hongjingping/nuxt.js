a4224b977d383d26abb41d345635dd940011cc9c
wikihong <hjpChinash@163.com>
2018-04-25 13:51:06 +0800

    .subtitle {

---
+++ pages/about/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>about Index page</h2>
+    <ul>
+      <li><a href="/">Home</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/news/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>news Index page</h2>
+    <ul>
+      <li><a href="/">News</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
